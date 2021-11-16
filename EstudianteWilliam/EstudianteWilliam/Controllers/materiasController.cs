fb731aabc0511428ab89b7c36df94335bf3addfb
William Diaz <williamefrain1994@gmail.com>
2021-11-16 15:23:00 -0600

    go

---
+++ EstudianteWilliam/EstudianteWilliam/Controllers/materiasController.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using EstudianteWilliam.Models;
+
+namespace EstudianteWilliam.Controllers
+{
+    public class materiasController : Controller
+    {
+        private EstudiantesEntities db = new EstudiantesEntities();
+
+        // GET: materias
+        public ActionResult Index()
+        {
+
+            return View(db.materia.ToList());
+        }
+
+        // GET: materias/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            materia materia = db.materia.Find(id);
+            if (materia == null)
+            {
+                return HttpNotFound();
+            }
+            return View(materia);
+        }
+
+        // GET: materias/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: materias/Create
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que quiere enlazarse. Para obtener 
+        // más detalles, vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "idMateria,nombreMateria")] materia materia)
+        {
+            if (ModelState.IsValid)
+            {
+                db.materia.Add(materia);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(materia);
+        }
+
+        // GET: materias/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            materia materia = db.materia.Find(id);
+            if (materia == null)
+            {
+                return HttpNotFound();
+            }
+            return View(materia);
+        }
+
+        // POST: materias/Edit/5
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que quiere enlazarse. Para obtener 
+        // más detalles, vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit( materia materia)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(materia).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(materia);
+        }
+
+        // GET: materias/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            materia materia = db.materia.Find(id);
+            if (materia == null)
+            {
+                return HttpNotFound();
+            }
+            return View(materia);
+        }
+
+        // POST: materias/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            materia materia = db.materia.Find(id);
+            db.materia.Remove(materia);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
